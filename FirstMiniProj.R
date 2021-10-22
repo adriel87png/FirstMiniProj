ed233b47de1298762feb35be56b2878acb96ef96
Adriel <jmrabe@up.edu.ph>
2021-10-22 17:04:06 +0800

    Added comments for #1

--- FirstMiniProj.R
+++ FirstMiniProj.R
@@ -2,6 +2,38 @@
 #Code by Jett Adriel M. Rabe
 #CMSC 197-2 Data Science
 
+#Creates a function named pollutantmean, takes in parameters:
+#directory -> pathway to file
+#pollutant -> name of pollutant
+#id -> an integer vector
 pollutantmean <- function(directory, pollutant, id=1:332){
-  files_list <- list.files()
+  
+  #Creates a list of files, full.names set to true to get the folder name as well
+  files_list <- list.files(directory, full.names = TRUE)
+  
+  #This creates an empty data frame
+  mydataframe <- data.frame()
+  
+  #for loop that loops through the files_list, made possible with id
+  for (i in id){
+    #Extends the data frame by using rbind to bind the succeeding csv files
+    mydataframe <- rbind(mydataframe, read.csv(files_list[i]))
+  }
+  
+  #takes the mean of pollutant specified from within the dataframe and stores it
+  #na.rm = TRUE allows us to ignore NA values
+  resultmean <- mean(mydataframe[, pollutant], na.rm=TRUE)
+  
+  #Simply returns the result stored
+  return (resultmean)
 }
+
+#Samples
+
+pollutantmean("specdata", "sulfate", 1:10) #Ans: 4.064128
+
+#For this particular sample, I am quite certain that the answer is
+#different as opposed to what is shown in the example output given to us
+pollutantmean("specdata", "nitrate", 70:72) #Ans: 1.706047
+
+pollutantmean("specdata", "nitrate", 23) #Ans: 1.280833
